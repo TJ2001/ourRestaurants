8a4df94a6abae533b3f262033558c2c56adc779a
Karen Freeman-Smith and Tim Jung <karen+tim@>
2016-11-14 13:39:38 -0800

    add new font

--- app/src/main/java/com/example/guest/ourrestaurants/MainActivity.java
+++ app/src/main/java/com/example/guest/ourrestaurants/MainActivity.java
@@ -8,17 +8,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.graphics.Typeface;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindRestaurantsButton;
     private EditText mLocationEditText;
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mLocationEditText = (EditText) findViewById(R.id.editText);
+        mAppNameTextView = (TextView) findViewById(R.id.textViewAmazing);
+        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/Anagram.ttf");
+        mAppNameTextView.setTypeface(ostrichFont);
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener(){
             @Override
