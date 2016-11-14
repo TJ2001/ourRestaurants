efe97b4147224cd4f67ebfab8446337c6ae2fc91
Karen Freeman-Smith and Tim Jung <karen+tim@>
2016-11-14 15:08:29 -0800

    add RestaurantsActivityInstrumentationTest

--- app/src/main/java/com/example/guest/ourrestaurants/MainActivity.java
+++ app/src/main/java/com/example/guest/ourrestaurants/MainActivity.java
@@ -1,14 +1,13 @@
 package com.example.guest.ourrestaurants;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.graphics.Typeface;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
