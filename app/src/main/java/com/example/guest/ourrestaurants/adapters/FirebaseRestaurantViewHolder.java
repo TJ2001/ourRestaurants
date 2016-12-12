a6c3581492fd908ee3e0646e457dfa6a51a183bf
TJ2001 <jung.m.tim@gmail.com>
2016-12-12 11:23:37 -0800

    add animation

--- app/src/main/java/com/example/guest/ourrestaurants/adapters/FirebaseRestaurantViewHolder.java
+++ app/src/main/java/com/example/guest/ourrestaurants/adapters/FirebaseRestaurantViewHolder.java
@@ -1,8 +1,12 @@
 package com.example.guest.ourrestaurants.adapters;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -11,6 +15,7 @@ import com.example.guest.ourrestaurants.Constants;
 import com.example.guest.ourrestaurants.R;
 import com.example.guest.ourrestaurants.models.Restaurant;
 import com.example.guest.ourrestaurants.ui.RestaurantDetailActivity;
+import com.example.guest.ourrestaurants.util.ItemTouchHelperViewHolder;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,7 +27,7 @@ import org.parceler.Parcels;
 
 import java.util.ArrayList;
 
-public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder {
+public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder implements ItemTouchHelperViewHolder{
     private static final int MAX_WIDTH = 200;
     private static final int MAX_HEIGHT = 200;
     public ImageView mRestaurantImageView;
@@ -52,4 +57,28 @@ public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder {
         categoryTextView.setText(restaurant.getCategories().get(0));
         ratingTextView.setText("Rating: " + restaurant.getRating() + "/5");
     }
+
+    @Override
+    public void onItemSelected() {
+//        itemView.animate()
+//                .alpha(0.7f)
+//                .scaleX(0.9f)
+//                .scaleY(0.9f)
+//                .setDuration(500);
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(mContext, R.animator.drag_scale_on);
+        set.setTarget(itemView);
+        set.start();
+    }
+
+    @Override
+    public void onItemClear() {
+//        itemView.animate()
+//                .alpha(1f)
+//                .scaleX(1f)
+//                .scaleY(1f);
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(mContext, R.animator.drag_scale_off);
+        set.setTarget(itemView);
+        set.start();
+    }
+
 }
---
+++ app/src/main/java/com/example/guest/ourrestaurants/util/ItemTouchHelperViewHolder.java
@@ -0,0 +1,9 @@
+package com.example.guest.ourrestaurants.util;
+
+/**
+ * Created by Guest on 12/12/16.
+ */
+public interface ItemTouchHelperViewHolder {
+    void onItemSelected();
+    void onItemClear();
+}
